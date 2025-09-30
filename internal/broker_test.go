eb443d6a174c14acedc0231e473c44edd6152fb7
YaroslavalsoraY <jaroslavpodoprigora@yandex.ru>
2025-09-30 08:11:43 +0300

    add kafka tests

--- internal/broker_test.go
+++ internal/broker_test.go
@@ -2,6 +2,7 @@ package internal_test
 
 import (
 	"message_broker/internal/channels"
+	kafkaBroker "message_broker/internal/kafka"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -19,6 +20,28 @@ func TestChannels(t *testing.T) {
 
 	for i := 0; i < 4; i++ {
 		value, _ := ch.Receive()
+		result = append(result, string(value))
+	}
+
+	assert.Equal(t, len(testValues), len(result))
+	for i := 0; i < 4; i++ {
+		assert.Equal(t, testValues[i], result[i])
+	}
+}
+
+func TestKafka(t *testing.T) {
+	broker, err := kafkaBroker.NewKafkaBroker("localhost:9092", "test")
+	assert.Equal(t, nil, err)
+
+	testValues := []string{"Hello", "World", "Apple", "Banana"}
+	result := make([]string, 0, 4)
+
+	for _, el := range testValues{
+		broker.Send([]byte(el))
+	}
+
+	for i := 0; i < 4; i++ {
+		value, _ := broker.Receive()
 		result = append(result, string(value))
 	}
 
